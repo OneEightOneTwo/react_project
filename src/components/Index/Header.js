c85616a6b9e096086076658f8f4dbfd60fbf3197
xiaocai <1299091267@qq.com>
2019-05-04 14:09:23 +0800

    cc

--- src/components/Index/Header.js
+++ src/components/Index/Header.js
@@ -11,17 +11,20 @@ class Header extends Component {
 
     render() {
         return (
-            <header className={styles.Header}>
-                <div className={styles.addBox}>
-                    <img src="https://img.ddky.com/c/wap/images/ddky2/locaton_icon.png" alt=""/>
-                    <span>叮当智慧药房（广州）</span>
-                    <img src="https://img.ddky.com/c/wap/images/ddky2/down_arrow.png" alt=""/>
-                </div>
-                <div className={styles.searchBox}>
-                    <input className={styles.input} placeholder="搜索药品、症状、品牌"></input>
-                    <img src="https://img.ddky.com/c/wap/images/ddky2/big_glass.png" alt=""/>
-                </div>
-            </header>
+            <div style={{height:"100%",height:"45px"}}>
+                <header className={styles.Header} >
+                    <div className={styles.addBox}>
+                        <img src="https://img.ddky.com/c/wap/images/ddky2/locaton_icon.png" alt=""/>
+                        <span>叮当智慧药房（广州）</span>
+                        <img src="https://img.ddky.com/c/wap/images/ddky2/down_arrow.png" alt=""/>
+                    </div>
+                    <div className={styles.searchBox}>
+                        <input className={styles.input} placeholder="搜索药品、症状、品牌"></input>
+                        <img src="https://img.ddky.com/c/wap/images/ddky2/big_glass.png" alt=""/>
+                    </div>
+                </header>
+            </div>
+
         )
     }
 }
